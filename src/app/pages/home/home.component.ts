e223145c47186d1e771e64a05cf736f74e6388fa
Ronan Coughlan <ronanski11@outlook.com>
2025-05-16 08:42:40 +0200

    Add functional home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { AppAuthService } from '../../service/app.auth.service';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterLink,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  isLoggedIn = false;
 
+  constructor(private authService: AppAuthService) {}
+
+  ngOnInit(): void {
+    // Check if user is authenticated
+    this.isLoggedIn = this.authService.isAuthenticated();
+
+    // Subscribe to any authentication changes
+    this.authService.accessTokenObservable.subscribe(() => {
+      this.isLoggedIn = this.authService.isAuthenticated();
+    });
+  }
 }
